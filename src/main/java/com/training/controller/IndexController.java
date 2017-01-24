69d066001defc0fd59fa69bdd8c4f1ab8b6ded31
igprad <ign.aldi.pradana@gmail.com>
2017-01-24 14:20:19 +0700

    Update iindex controller dan public index.html

--- src/main/java/com/training/controller/IndexController.java
+++ src/main/java/com/training/controller/IndexController.java
@@ -4,10 +4,13 @@
  * and open the template in the editor.
  */
 package com.training.controller;
-
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 /**
  *
@@ -15,9 +18,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class IndexController {
+//    @Autowired
+//    DataRepository datarepo;
     
-    @RequestMapping("/")
+    @RequestMapping(value="/", method=RequestMethod.GET)
     public String home(Model model){
         return "index";
     }
+    
+    @RequestMapping(value="/input",method = RequestMethod.GET)
+    public String inputName(
+            @RequestParam(value="data") String data,Model model
+    ){
+//        DataModel inputan = datarepo.getData();
+        model.addAttribute("message",data);
+        return "users/index";
+    }
+    
+
 }
