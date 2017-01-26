62b308f4cd921a5d7fc0349ab7cea4819683699f
igprad <ign.aldi.pradana@gmail.com>
2017-01-26 11:37:43 +0700

    Addedd Service for userServices

---
+++ src/main/java/com/training/services/UserServices.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.training.services;
+
+import com.training.model.User;
+import com.training.repository.UserRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author ALz
+ */
+public class UserServices{
+    
+    @Autowired
+    UserRepository userRepository;
+    
+    public void insert(User model){
+        userRepository.save(model);
+    }
+    
+    public void update(){}
+    public void delete(){}
+    
+    public boolean authLogin(String username,String password){
+        User result = userRepository.authLogin(username, password);
+        Boolean checkResult=true;
+        if(result==null)
+            checkResult=false;
+        return checkResult;
+    }
+    
+    public List<User>seeAllUsers(){
+        List<User> result;
+        result = userRepository.findAll();
+        return result;
+    }
+    
+}
